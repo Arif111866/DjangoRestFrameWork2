9901a0034342e78eced0a90a08a9a6301f692ff2
Ariful Islam <ariful111866@gmail.com>
2023-07-12 20:40:12 +0600

    creat a bread model

--- dog_app/models.py
+++ dog_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Bread(models.Model):
+    name = models.CharField(max_length=50)
+    size = models.CharField(max_length=50)
+    friendliness =models.IntegerField()
+    trainability = models.IntegerField()
+    sheddingamount = models.IntegerField()
+    exerciseneeds = models.IntegerField()
