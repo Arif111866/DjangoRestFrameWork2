d0b21e4ad94ccc562d13c904c52e1332e9a10ba3
Ariful Islam <ariful111866@gmail.com>
2023-07-13 21:34:03 +0600

    choices add korchi

--- dog_app/models.py
+++ dog_app/models.py
@@ -1,13 +1,28 @@
 from django.db import models
 
 # Create your models here.
+SIZE_CHOICES =(
+    ('Tiny' , 'Tiny'),
+    ('Small' ,'Small'),
+    ('Medium' , 'Medium'),
+    ('Large' , 'Large')
+)
+INTENSITY_CHOICES =(
+    (1,1),
+    (2,2),
+    (3,3),
+    (4,4),
+    (5,5)
+)
+
+
 class Bread(models.Model):
     name = models.CharField(max_length=50)
-    size = models.CharField(max_length=50)
-    friendliness =models.IntegerField()
-    trainability = models.IntegerField()
-    sheddingamount = models.IntegerField()
-    exerciseneeds = models.IntegerField()
+    size = models.CharField(max_length=50 , choices=SIZE_CHOICES)
+    friendliness =models.IntegerField(choices= INTENSITY_CHOICES)
+    trainability = models.IntegerField(choices=INTENSITY_CHOICES)
+    sheddingamount = models.IntegerField(choices=INTENSITY_CHOICES)
+    exerciseneeds = models.IntegerField(choices=INTENSITY_CHOICES)
     def __str__(self):
         return self.name
     
